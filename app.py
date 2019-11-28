f26ae40c33736270c6d1a76e7b34ae303deff277
Abhishar Sinha <sabhishar@gmail.com>
2019-11-28 16:53:42 +0530

    Finished a basic api.

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 # REFERENCES:
 # 1. https://flask.palletsprojects.com/en/1.1.x/patterns/fileuploads/
 # 2. https://stackoverflow.com/questions/17170752/python-opencv-load-image-from-byte-string
+# 3. https://medium.com/@jsflo.dev/saving-and-loading-a-tensorflow-model-using-the-savedmodel-api-17645576527
 
-# 'input_image_as_bytes:0'
 
-from flask import Flask, request, redirect
+
+from flask import Flask, request, redirect, jsonify
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
+import tensorflow as tf
+export_path = "./exported-model"
 
 app = Flask(__name__)
+
+sess = tf.Session(graph=tf.Graph())
+tf.saved_model.loader.load(sess, ["serve"], export_path)
 
 @app.route("/", methods=["POST", "GET"])
 def detect_text():
@@ -18,17 +24,20 @@ def detect_text():
             print("NO files")
         else:
             print("File got")
-        img = request.files["image"]
-        print("Image:", img.filename)
-        image = img.read()
-        # print(type(img.read()))
-        nparr = np.frombuffer(image, np.uint8)
-        img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        # plt.imshow(img_np)
-        # plt.show()
-        return redirect("file:///home/abhishar/Desktop/Abhishar_Sinha-IITB-Assignment/client.html")
+        files = request.files.getlist("image")
+        send_res = {"response":[]}
+        for img in files:
+            print("Image:", img.filename)
+            image = img.read()
+            out = sess.run(['prediction:0', 'probability:0'], feed_dict={'input_image_as_bytes:0': image})
+            print(out)
+            temp = {"filename":img.filename, "prediction":out[0].decode("utf-8"), "probability":out[1]}
+            send_res["response"].append(temp)
+        sess.close()
+        return jsonify(send_res)
     if request.method == "GET":
         return redirect("file:///home/abhishar/Desktop/Abhishar_Sinha-IITB-Assignment/client.html")
 
 if __name__ == "__main__":
     app.run(host="localhost")
+    # close the session properly
--- trying_model.py
+++ trying_model.py
@@ -6,9 +6,11 @@ export_path = "./exported-model"
 file = open("img.jpg", "rb")
 img = file.read()
 file.close()
-with tf.Session(graph=tf.Graph()) as sess:
-    tf.saved_model.loader.load(sess, ["serve"], export_path)
-    # graph = tf.get_default_graph()
-    # print(graph.get_operations())
-    sess.run(['prediction:0', 'probability:0'],
-               feed_dict={'input_image_as_bytes:0': img})
+# with tf.Session(graph=tf.Graph()) as sess:
+sess = tf.Session(graph=tf.Graph())
+tf.saved_model.loader.load(sess, ["serve"], export_path)
+# graph = tf.get_default_graph()
+# print(graph.get_operations())
+sess.run(['prediction:0', 'probability:0'], feed_dict={'input_image_as_bytes:0': img})
+
+sess.close()
